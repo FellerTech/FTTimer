c074e6038a19315b5794821a514c5b3769bb6d62
Steve Feller <sfeller@fellertech.net>
2022-05-13 14:56:44 -0400

    Initial gtest in place

--- tests/FTTimerTest.cpp
+++ tests/FTTimerTest.cpp
@@ -1,9 +1,39 @@
+#include <chrono>
+#include <thread>
 
 #include <FTTimer.hpp>
 #include <gtest/gtest.h>
 //#include <gtest.h>
 
-TEST( FTTimerTest, Sample) { EXPECT_EQ(1, FTTimer::getTime()); };
+/**
+ * \brief tests te given time delay
+ * \param [in] delay time to delay in microseconds
+ * \return time between start in milliseconds
+ */
+double timeDelay( long delay) {
+  double start = FTTimer::getTime();
+  std::this_thread::sleep_for(std::chrono::microseconds(delay));
+  double end = FTTimer::getTime();
+
+  return (end-start) * 1e6;
+}
+
+::testing::AssertionResult IsBetweenInclusive(double val, double a, double b)
+{
+    if((val >= a) && (val <= b))
+        return ::testing::AssertionSuccess();
+    else
+        return ::testing::AssertionFailure()
+               << val << " is outside the range " << a << " to " << b;
+};
+
+//Test timer accuracy
+TEST( FTTimerTest, delay) {
+  double range = 100;
+  double delay = 100;
+  double result = timeDelay(delay);
+  EXPECT_TRUE( IsBetweenInclusive( result, delay - range, delay + range ));
+}
 
 int main(int argc, char **argv)
 {
